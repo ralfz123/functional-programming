edd0a6e9fccdf4ab6df93cccf1ffd1914008fdf3
ralfz123 <ralf_zonneveld@hotmail.com>
2021-01-27 20:04:47 +0100

    Turn dev mode off

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ let capitalizeNames = checkedNames.map((name) => {
 
 // Combine data keys 'tijdreisJaar' & 'waaromTijdreizenNaarJaar' and nest them in a self-made data key `tijdreis`. It also replaces empty values with `null`
 let nestedData = nestDataKeys(myData);
-console.log('Nested data:', nestedData);
+// console.log('Nested data:', nestedData);
 
 function nestDataKeys(data) {
   const dataObject = data.map((element) => {
